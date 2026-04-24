1bdeec5650c8252d854e9b4ebc89b8b9dc8ac61d
Aslin Santhosh <your-email@example.com>
2026-04-24 11:07:08 +0530

    Warning from vercel

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,18 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('framer-motion')) return 'vendor_framer';
+            if (id.includes('gsap')) return 'vendor_gsap';
+            if (id.includes('lucide-react')) return 'vendor_icons';
+            return 'vendor';
+          }
+        },
+      },
+    },
   },
   server: {
     host: true,
